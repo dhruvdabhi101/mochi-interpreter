eab570afeda9cf57f0a9fbc0d49dadb270c222e5
Dhruv Dabhi <dhruvdabhi101@gmail.com>
2024-09-10 20:45:09 +0530

    added simple repl

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/dhruvdabhi101/interpreter/repl"
+)
+
+func main() {
+	user, err := user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey Programming language! Learn with Dhruv!\n", user.Username)
+
+	fmt.Println("Feel free to type in Commands")
+	repl.Start(os.Stdin, os.Stdout)
+}
