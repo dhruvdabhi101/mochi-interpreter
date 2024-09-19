4a37eca8d13e77a06663d40ca91020a54790830e
Dhruv Dabhi <dhruvdabhi101@gmail.com>
2024-09-19 18:00:57 +0530

    added function parsing

--- ast/ast.go
+++ ast/ast.go
@@ -2,6 +2,8 @@ package ast
 
 import (
 	"bytes"
+	"strings"
+
 	"github.com/dhruvdabhi101/interpreter/token"
 )
 
@@ -208,3 +210,30 @@ func (bs *BlockStatement) String() string {
 	}
 	return out.String()
 }
+
+type FunctionLiteral struct {
+	Token      token.Token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode() {}
+func (fl *FunctionLiteral) TokenLiteral() string {
+	return fl.Token.Literal
+}
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
+	return out.String()
+}
+
